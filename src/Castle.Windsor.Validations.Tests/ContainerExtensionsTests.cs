ec13a8d088a040b54b20ca460a95c223f1565976
Oshry Horn <HornOshry@gmail.com>
2019-09-16 18:18:13 +0100

    #6 - Added a new basic skeleton test project

---
+++ src/Castle.Windsor.Validations.Tests/ContainerExtensionsTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Castle.Windsor.Validations.Tests
+{
+    public class ContainerExtensionsTests
+    {
+        [Test]
+        public void ValidateAllDependenciesResolvableAsync_Fail()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ValidateAllDependenciesResolvableAsync_Success()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ src/Castle.Windsor.Validations.Tests/InstallerExtensionsTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Castle.Windsor.Validations.Tests
+{
+    public class InstallerExtensionsTests
+    {
+        [Test]
+        public void ValidateAllDependenciesResolvableAsync_Fail()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ValidateAllDependenciesResolvableAsync_Success()
+        {
+            Assert.Pass();
+        }
+    }
+}
