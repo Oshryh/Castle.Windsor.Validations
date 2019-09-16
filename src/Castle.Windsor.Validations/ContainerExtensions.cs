dd3dcb300e788eeb002c2477adc1fff33b0dd46a
Oshry Horn <HornOshry@gmail.com>
2019-09-16 08:36:20 +0100

    Renamed solution, project, namespace and classes. Reorganized

--- src/Castle.Windsor.Validations/ContainerExtensions.cs
+++ src/Castle.Windsor.Validations/ContainerExtensions.cs
@@ -2,30 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Castle.MicroKernel.Registration;
 
-namespace Castle.Windsor.DependenciesValidations
+namespace Castle.Windsor.Validations
 {
-    public static class Extentions 
+    public static class ContainerExtensions 
     {
-
-        public static async Task ValidateAllDependenciesResolvableAsync(this IWindsorInstaller installer)
-        {
-            using (var container = new WindsorContainer())
-            {
-                container.Install(installer);
-                await ValidateAllDependenciesResolvableAsync(container).ConfigureAwait(false);
-            }
-        }
-
-        public static void ValidateAllDependenciesResolvable(this IWindsorInstaller installer)
-        {
-            using (var container = new WindsorContainer())
-            {
-                container.Install(installer);
-                ValidateAllDependenciesResolvableAsync(container).Wait();
-            }
-        }
 
         public static void ValidateAllDependenciesResolvable(this IWindsorContainer container)
         {
