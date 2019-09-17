e6bb729ee14f2b0e1c5025258922fce1de62cb86
Oshry Horn <HornOshry@gmail.com>
2019-09-17 07:42:18 +0100

    #2 - Added a test for the WcfFacility bug. Once the test passes, the bug is resolved.

---
+++ src/Castle.Windsor.Validations.Tests/WcfFacilityBugTest/BugTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Castle.Windsor.Validations;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Castle.Windsor.Validations.Tests.WcfFacilityBugTest
+{
+    public class WcfFacilityBugTests
+    {
+
+        [Test]
+        public void ResolveInstallerWithWcfFacilityTest()
+        {
+            var action = ((Action)(() => new WcfFacilityTestInstaller().ValidateAllDependenciesResolvable()));
+            action.Should().NotThrow();
+        }
+    }
+}
---
+++ src/Castle.Windsor.Validations.Tests/WcfFacilityBugTest/WcfFacilityTestInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Castle.Facilities.WcfIntegration;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+
+namespace Castle.Windsor.Validations.Tests.WcfFacilityBugTest
+{
+    public class WcfFacilityTestInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.AddFacility<WcfFacility>();
+        }
+    }
+}
